9890525ed0c3bf0000a43207e8c673905f304517
misudesu <misaeldessalegn@gmail.com>
2024-10-07 16:23:09 +0300

    db name

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'event',
     bufferCommands: false,
   })
 
