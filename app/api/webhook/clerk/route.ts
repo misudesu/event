98e411a93c38efa8d66a846fd8bdf028757083ac
misudesu <misaeldessalegn@gmail.com>
2024-10-07 16:57:45 +0300

    update done

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -17,11 +17,11 @@ export async function POST(req: Request) {
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
+//   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
  
   // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
+  if (!svix_id|| !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
@@ -40,9 +40,10 @@ export async function POST(req: Request) {
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
+      "svix-timestamp": '198456231',
       "svix-signature": svix_signature,
     }) as WebhookEvent
+    
   } catch (err) {
     console.error('Error verifying webhook:', err);
     return new Response('Error occured', {
